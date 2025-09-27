de44f9bf9f56d8b1139788a9d93229e40833e65b
황정빈 <hjbin1211@gmail.com>
2025-09-27 20:46:53 +0900

    initial commit

---
+++ src/test/java/com/louter/uhd/UhdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.louter.uhd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UhdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
