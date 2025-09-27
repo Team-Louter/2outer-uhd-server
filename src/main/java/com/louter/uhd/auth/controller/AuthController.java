5bd895297561ad0c5a03b4fdd5c0180471b31e7f
황정빈 <hjbin1211@gmail.com>
2025-09-27 23:43:47 +0900

    feat : EmailSender controller part addition

--- src/main/java/com/louter/uhd/auth/controller/AuthController.java
+++ src/main/java/com/louter/uhd/auth/controller/AuthController.java
@@ -1,9 +1,13 @@
 package com.louter.uhd.auth.controller;
 
 import com.louter.uhd.auth.domain.User;
+import com.louter.uhd.auth.dto.request.SendVerificationEmailRequest;
 import com.louter.uhd.auth.dto.request.SignupRequest;
+import com.louter.uhd.auth.dto.response.SendVerificationEmailResponse;
 import com.louter.uhd.auth.dto.response.SignupResponse;
+import com.louter.uhd.auth.usecase.EmailUseCase;
 import com.louter.uhd.auth.usecase.SignupUseCase;
+import jakarta.validation.constraints.Email;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,11 +15,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
+    // 회원가입
     private final SignupUseCase signupUseCase;
+    // 이메일
+    private final EmailUseCase emailUseCase;
+
+    @PostMapping("/email/send")
+    public ResponseEntity<Object> signupEmail(@RequestBody SendVerificationEmailRequest request) {
+        Map<User, String> response = emailUseCase.sendVerificationEmail(request);
+        return ResponseEntity.ok(SendVerificationEmailResponse.from(response));
+    }
 
     @PostMapping("/signup")
     public ResponseEntity<Object> signup(@RequestBody SignupRequest signupRequest) {
